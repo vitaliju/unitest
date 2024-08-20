64a2ac268824edbe635f316d0cff47e12c4b7c0c
vitaliju <167984771+vitaliju@users.noreply.github.com>
2024-08-20 12:22:23 +0300

    Test update

--- sum.js
+++ sum.js
@@ -1,4 +1,30 @@
 function sum(a, b) {
     return a + b;
 }
-module.exports = sum;
+
+function klonas(x) {
+    return [...x];
+}
+
+function amzius(x) {
+    if (x < 0) {
+        throw Error();
+    }
+    return x * 365;
+}
+function string(text) {
+    return text.split('').reverse().join('');
+}
+function isPalindrome(string) {
+    if (string.split('').reverse().join('') === string) {
+        return string;
+    }
+}
+
+module.exports = {
+    sum,
+    klonas,
+    amzius,
+    string,
+    isPalindrome
+};
--- sum.test.js
+++ sum.test.js
@@ -1,9 +1,47 @@
-const sum = require('./sum');
+const { sum, klonas, amzius, string, isPalindrome } = require("./sum");
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+describe("Sum tests", () => {
+    test("adds 1 + 1 to equal 2", () => {
+        expect(sum(1, 1)).toBe(2);
+    });
+
+    test("adds 1 + 2 to equal 3", () => {
+        expect(sum(1, 2)).toEqual(3);
+    });
 });
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 4)).toBe(3);
+describe("Klonas tests", () => {
+    test("array should be cloned", () => {
+        let x = [1, 2, 3];
+        expect(klonas(x)).not.toBe(x);
+    });
+
+    test("array should contain the same elements", () => {
+        expect(klonas([1, 2, 3])).toEqual([1, 2, 3]);
+    });
+});
+
+describe("Amzius tests", () => {
+    test("age of 1 should return 365 days", () => {
+        expect(amzius(1)).toBe(365);
+    });
+
+    test("negative age should return an error", () => {
+        const fun = () => {
+            amzius(-1);
+        };
+        expect(fun).toThrow();
+    });
+});
+
+describe('String tests', () => {
+    test('Labas reverse to equal sabaL', () => {
+        expect(string('Labas')).toBe('sabaL');
+    });
+});
+
+describe('isPalindrome tests', () => {
+    test('text is palindrome', () => {
+        expect(isPalindrome('level')).toBe('level');
+    });
 });
